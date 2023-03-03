8465ae550c11a8624fc45d481a654028c09fa7d5
Edouard de Romree de Vichenet <deromree.edouard@gmail.com>
2023-03-03 16:08:39 +0100

    améliorations

--- resources/views/clients/edit.blade.php
+++ resources/views/clients/edit.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Modification client</title>
     @vite('resources/css/app.css')
 </head>
 <body>
@@ -33,7 +33,7 @@
     <footer class="bg-gray-200 text-white mt-20">
         <div class="container mx-auto py-6 px-4">
             <div class="lex items-center justify-between">
-            <div class="text-center mb-4 md:mb-0">
+            <div class="text-center mb-4 ">
                 <p class="text-black">&copy; 2023 - Réalisé par Edouard de Romrée de Vichenet</p>
             </div>
             </div>
--- resources/views/factures/index.blade.php
+++ resources/views/factures/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Factures</title>
+    <title>Mes factures</title>
     @vite('resources/css/app.css')
 </head>
 <body>
@@ -49,7 +49,7 @@
         </tbody>
         @endforeach
     </table>
-    <a href="/home" class="ml-16 mt-8 inline-block bg-red-500 hover:bg-red-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Retour à l'accueil</a>
+    <a href="/" class="ml-16 mt-8 inline-block bg-red-500 hover:bg-red-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Retour à l'accueil</a>
     <a href="/factures/create" class="ml-12 mt-8 inline-block bg-red-500 hover:bg-red-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Ajouter une facture</a>
     <footer class="bg-gray-200 text-white mt-20">
         <div class="container mx-auto py-6 px-4">
